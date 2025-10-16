a4563f4565411b3db35c58cf009fed048c831bb9
Denise Soriano <denisedsoriano@gmail.com>
2025-10-16 14:01:40 +0800

    Update homepage

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,15 +1,12 @@
-import { useState } from 'react'
 import reactLogo from './../assets/react.svg'
 import viteLogo from '/vite.svg'
 import './../App.css'
 import { Link } from 'react-router-dom'
 
 function Home() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
+      <div className='flex flex-row'>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -17,15 +14,7 @@ function Home() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
+      <h1>Mini-React Projects</h1>
       <div className='flex flex-col gap-3'>
         <Link to='/todo-list'><button className='link'>To-Do List</button></Link>
         <Link to='/weather-dashboard'><button className='link'>Weather Dashboard</button></Link>
