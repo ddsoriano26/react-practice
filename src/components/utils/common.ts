9f471421e8934d1995303fb4a0af9755ec3b6bcb
Denise Soriano <denisedsoriano@gmail.com>
2025-10-23 03:17:36 +0800

    Update throttle function to accommodate type any

--- src/components/utils/common.ts
+++ src/components/utils/common.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 // Transform YYYY-MM-DD to MMM DD, YYYY
 export function formatDate(dateString: string): string {
     const date = new Date(dateString);
@@ -12,20 +13,21 @@ export function formatDate(dateString: string): string {
   }
 
 // Basic throttle function
-export function throttle<T extends (...args: unknown[]) => void>(
-    func: T,
-    limit: number
-  ): (...args: Parameters<T>) => void {
-    let inThrottle = false;
-  
-    return function (this: unknown, ...args: Parameters<T>) {
-      if (!inThrottle) {
-        func.apply(this, args);
-        inThrottle = true;
-        setTimeout(() => (inThrottle = false), limit);
-      }
-    };
-  }
+export function throttle<T extends (...args: any[]) => any>(
+  func: T,
+  limit: number
+): (...args: Parameters<T>) => void {
+  let inThrottle = false;
+
+  return function (this: unknown, ...args: Parameters<T>) {
+    if (!inThrottle) {
+      func.apply(this, args);
+      inThrottle = true;
+      setTimeout(() => (inThrottle = false), limit);
+    }
+  };
+}
+
 
 // Basic debounce function
 export function debounce<T extends (...args: unknown[]) => void>(
